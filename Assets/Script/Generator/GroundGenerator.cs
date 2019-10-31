db6063a94b16ebfa47919f247525728f70df77f5
James Chuang <jameschuang1111@gmail.com>
2019-10-30 19:30:33 -0700

    generators set up

---
+++ Assets/Script/Generator/GroundGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGenerator : MonobehaviorSingleton<GroundGenerator>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
