7047aa03f463763e958b5357476ed198516bb9ff
James Chuang <jameschuang1111@gmail.com>
2019-11-06 18:44:26 -0800

    added deprecated ter. gen.

---
+++ Assets/Script/Util/FullSreenSpriteScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullSreenSpriteScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        SpriteRenderer spriteRenderer = this.gameObject.GetComponent<SpriteRenderer>();
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        Vector2 cameraSize = new Vector2(Camera.main.aspect * cameraHeight, cameraHeight);
+        Vector2 spriteSize = spriteRenderer.sprite.bounds.size;
+    }
+}
