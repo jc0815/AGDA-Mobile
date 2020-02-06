33cc877a910bc3793337ab8cf3875540f2adb05e
winton <wenhong.winton.zhang@gmail.com>
2020-02-06 11:48:33 -0800

    OK

--- Assets/Script/Generator/ObstacleGenerator.cs
+++ Assets/Script/Generator/ObstacleGenerator.cs
@@ -16,12 +16,14 @@ public class ObstacleGenerator : MonobehaviorSingleton<ObstacleGenerator>
     private GameObject block;
     private GameObject blocktwo;
     private GameObject screenBlock;
+    private GameObject detector;
     private int width = 0;
     private int height = 0;
     public bool[,] blocks = new bool[21, 13];
     public bool[,] topBlocks = new bool[21, 13];
     private float whenToDissappear;
-    private float speed = 10.0f;
+    private float placeToDestoryDector;
+    private float speed = 10f;
     // Start is called before the first frame update
 
     void Awake()
@@ -29,35 +31,39 @@ public class ObstacleGenerator : MonobehaviorSingleton<ObstacleGenerator>
         //Generate();
         block = Resources.Load<GameObject>("Prefab/GroundBlock");
         blocktwo = Resources.Load<GameObject>("Prefab/GroundBlock");
+        detector = Resources.Load<GameObject>("Prefab/ground-individual");
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         whenToDissappear = -1.2f * screenBounds.x;
-        width = (int)(screenBounds.x);
+        placeToDestoryDector = screenBounds.x;
+        width = (int)(screenBounds.x) + 1;
         height = (int)(screenBounds.y);
         screenBlock = GameObject.FindGameObjectWithTag("Obstacle");
-        Debug.Log("found screenBlock?" + screenBlock);
+        // Debug.Log("found screenBlock?" + screenBlock);
     }
     // Start is called before the first frame update
     void Generate()
     {
+        int offset = width;
         //from boottom to up
         //Debug.Log("the width" + width);
         for (int y = 0; y < height; ++y)
         {
             for (int x = 0; x < width; ++x)
             {
+
                 blocks[x, y] = false;
                 //Debug.Log("the x is " + x + " the y is " + y);
                 bool isGenerate = Random.Range(1, 3) > 1 ? true : false;
                 if (y == 0 && isGenerate)
                 {
-                    GameObject childBlock = Instantiate(block, new Vector3(x, y + 1, 0), Quaternion.identity);
+                    GameObject childBlock = Instantiate(block, new Vector3(x + offset, y + 1, 0), Quaternion.identity);
                     blocks[x, y] = true;
                     childBlock.transform.SetParent(screenBlock.transform);
                 }
                 if (y != 0 && hasNeighborBottom(x, y) && isGenerate)
                 {
                     //Debug.Log("the y is " + y);
-                    GameObject childBlock = Instantiate(block, new Vector3(x, y + 1, 0), Quaternion.identity);
+                    GameObject childBlock = Instantiate(block, new Vector3(x + offset, y + 1, 0), Quaternion.identity);
                     blocks[x, y] = true;
                     childBlock.transform.SetParent(screenBlock.transform);
                 }
@@ -73,19 +79,20 @@ public class ObstacleGenerator : MonobehaviorSingleton<ObstacleGenerator>
                 bool isGenerate = Random.Range(1, 3) > 1 ? true : false;
                 if (y == height - 1 && isGenerate)
                 {
-                    GameObject childBlock = Instantiate(blocktwo, new Vector3(x, y + 1, 0), Quaternion.identity);
+                    GameObject childBlock = Instantiate(blocktwo, new Vector3(x + offset, y + 1, 0), Quaternion.identity);
                     topBlocks[x, y] = true;
                     childBlock.transform.SetParent(screenBlock.transform);
                 }
                 if (y != 0 && hasNeighborUp(x, y) && isGenerate)
                 {
                     //Debug.Log("the y is " + y);
-                    GameObject childBlock = Instantiate(blocktwo, new Vector3(x, y + 1, 0), Quaternion.identity);
+                    GameObject childBlock = Instantiate(blocktwo, new Vector3(x + offset, y + 1, 0), Quaternion.identity);
                     topBlocks[x, y] = true;
                     childBlock.transform.SetParent(screenBlock.transform);
                 }
             }
         }
+        Instantiate(detector, new Vector3(width * 2, height, 0), Quaternion.identity);
     }
 
     public void Destroy() //destroy object after it passes a certian point
@@ -97,7 +104,10 @@ public class ObstacleGenerator : MonobehaviorSingleton<ObstacleGenerator>
             if (x <= whenToDissappear)
                 Destroy(toDestroy[i]);
         }
-
+        GameObject detector = GameObject.FindGameObjectWithTag("detector");
+        float detectorPosition = detector.transform.position.x;
+        if (detectorPosition <= placeToDestoryDector)
+            Destroy(detector);
     }
 
 
@@ -154,6 +164,32 @@ public class ObstacleGenerator : MonobehaviorSingleton<ObstacleGenerator>
         {
             blocks = new bool[21, 13];
         }
-        screenBlock.GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
+        // screenBlock.GetComponent<Rigidbody2D>().velocity += new Vector2(speed, 0);
+        // screenBlock.transform.localPosition = new Vector2(width, 0);
+        if (!hasDetector())
+        {
+            this.Generate();
+        }
+        move();
+        Destroy();
+    }
+    void move()
+    {
+        GameObject[] ter = GameObject.FindGameObjectsWithTag("Ground");
+        GameObject detector = GameObject.FindGameObjectWithTag("detector");
+        detector.GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
+        for (int i = 0; i < ter.Length; i++)
+            ter[i].GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0);
+
+        // whenToDissappear = -1.2f * screenBounds.x;
+    }
+    bool hasDetector()
+    {
+        GameObject detector = GameObject.FindGameObjectWithTag("detector");
+        if (detector == null)
+        {
+            return false;
+        }
+        return true;
     }
 }
